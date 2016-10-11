ec41fedfa52e23202d0808083345548108f045ea
sreekarsai08 <sreekarsai08@gmail.com>
2016-10-11 10:24:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone',function(req,res){
-    res.sendFile('article one link served here');
+    res.send('article one link served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
