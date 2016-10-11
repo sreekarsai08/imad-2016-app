b80c9ec56815f1da2a3cd84af11f9e49ad7a3f8a
sreekarsai08 <sreekarsai08@gmail.com>
2016-10-11 10:23:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/articleone',function(req,res){
+    res.sendFile('article one link served here');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
