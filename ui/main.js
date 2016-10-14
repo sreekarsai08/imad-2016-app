6be9380f453c776c1a47113273a277b642546915
sreekarsai08 <sreekarsai08@gmail.com>
2016-10-14 19:09:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,15 @@ button.onclick=function(){
      var span=document.getElementById('count');
       span.innerHTML=counter.toString();
 };
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list +='<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
