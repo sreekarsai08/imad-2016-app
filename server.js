132089b4742df1b084da6892c7596d3dba10ac59
sreekarsai08 <sreekarsai08@gmail.com>
2016-10-23 14:41:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/counter',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/aboutschool',function(req,res){
+app.get('//aboutschool',function(req,res){
     res.sendFile(path.join(__dirname,"aboutschool.html"));
 });
 app.get('//ac',function(req, res){
