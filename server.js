12b28d727455c6e5483cd53b9d015ae6d7bf617f
sreekarsai08 <sreekarsai08@gmail.com>
2016-10-25 19:41:43 +0530

    [imad-console] Updates server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/ac', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ac.html'));
+});
+app.get('/aboutschool', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'aboutschool.html'));
+});
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
