3213d7485f85501d7c3db04a7dba7b33e227076d
sreekarsai08 <sreekarsai08@gmail.com>
2016-10-25 20:34:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-app.get('/aboutschool', function (req, res) {
+app.get('//aboutschool', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aboutschool.html'));
 });
-app.get('/ac', function (req, res) {
+app.get('//ac', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ac.html'));
 });
 
