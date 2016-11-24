a0f282b3b3847193cf8b9144c6ff1b2438f85b46
sreekarsai08 <sreekarsai08@gmail.com>
2016-11-24 18:46:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
 
 var app = express();
 app.use(morgan('combined'));
@@ -9,6 +10,8 @@ app.get('/ui/main.js', function (req, res) {
 });
 app.get('', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/test-db',function(req,res){
 });
 app.get('/aboutschool', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aboutschool.html'));
