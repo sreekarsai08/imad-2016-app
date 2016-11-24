86429e08265f81049178509290187913acbc00d9
sreekarsai08 <sreekarsai08@gmail.com>
2016-11-24 18:58:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.get('', function (req, res) {
 });
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
+    pool.quiery('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 app.get('/aboutschool', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aboutschool.html'));
