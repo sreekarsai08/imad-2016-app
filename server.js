033bc58b7a41267b236ec607096df2041cd570ff
sreekarsai08 <sreekarsai08@gmail.com>
2016-11-25 10:45:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config ={
     user:'sreekarsai08',
     database:'sreekarsai08',
@@ -18,6 +19,17 @@ app.get('/ui/main.js', function (req, res) {
 app.get('', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function (req, res) {
+  var hashedString=hash(req.params.input,'this-is-sreekar');
+  res.send(hashedString);
+});
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
